1518f17542cb244f152145b79adc13c85b9416ae
JaylenbEgg <bjaylen502@gmail.com>
2024-08-06 16:04:28 +0000

    age verification

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello world!')
+  // console.log('Hello world!') 
+
+  // var prompt = prompt('what is your name?') 
+
+  // console.log(prompt)
+
+  function verifyAge (age) { var age = prompt('what is your age?');
+    console.log(age) 
+
+    
+    }
+
+var age = prompt('what is your age?');
+console.log(age) 
+
+if(age < 18) {alert ('you are a minor!') 
+} else if (age >= 18 && age <=60)  {alert('you are an adult!')
+ } else {alert('you are a senior citizen!')
+ }
