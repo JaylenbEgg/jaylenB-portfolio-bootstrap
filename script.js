927092cb0a61af8cd7884f8615c5c88b1b732cd7
JaylenbEgg <bjaylen502@gmail.com>
2024-08-07 16:49:55 +0000

    MORE SCRIPT

--- script.js
+++ script.js
@@ -6,14 +6,29 @@
 
   function verifyAge (age) { var age = prompt('what is your age?');
     console.log(age) 
-
-    
-    }
-
 var age = prompt('what is your age?');
 console.log(age) 
 
 if(age < 18) {alert ('you are a minor!') 
 } else if (age >= 18 && age <=60)  {alert('you are an adult!')
  } else {alert('you are a senior citizen!')
- }
+ } 
+
+    
+    }
+
+
+   function uploadAndIdentifyPlants() { 
+    // get the photo from the frontend
+    var photoInput = document.getElementById('photoInput');
+    console.log(photoInput)
+     console.log(photoInput.files[0])
+    
+    // if no photo selected and user click submit let them know they  need to upload photo    
+    if (photoInput.files.length ==0 ) { 
+      alert('please select a photo to upload'); 
+      return;
+     }
+     var selectedfile = photoInput.files[0];
+}
+
